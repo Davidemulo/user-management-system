e36c674fe8a85a281c527b7917243499b11cf269
Davidemulo <emulodavid@gmail.com>
2026-06-07 18:19:35 +0100

    setup redux toolkit and fetch users from api

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,7 +1,34 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../store/userSlice";
+import { RootState } from "../store/store";
+
 export default function Users() {
+  const dispatch = useDispatch();
+  const { users, loading, error } = useSelector(
+    (state: RootState) => state.users
+  );
+
+  useEffect(() => {
+    dispatch(fetchUsers() as any);
+  }, [dispatch]);
+
   return (
     <div>
       <h1>Users List Page</h1>
+
+      {loading && <p>Loading users...</p>}
+      {error && <p>{error}</p>}
+
+      {users.map((user) => (
+        <div key={user.id} style={{ border: "1px solid gray", margin: 10 }}>
+          <h3>{user.name}</h3>
+          <p>{user.email}</p>
+          <p>
+            {user.address.city}, {user.address.street}
+          </p>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/store/userSlice.ts
@@ -0,0 +1,58 @@
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// Types
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  address: {
+    city: string;
+    street: string;
+  };
+}
+
+interface UserState {
+  users: User[];
+  loading: boolean;
+  error: string | null;
+}
+
+const initialState: UserState = {
+  users: [],
+  loading: false,
+  error: null,
+};
+
+// 🔥 Async thunk to fetch users
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+  return response.data;
+});
+
+const userSlice = createSlice({
+  name: "users",
+  initialState,
+  reducers: {
+    // we will add CRUD later
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUsers.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchUsers.fulfilled, (state, action: PayloadAction<User[]>) => {
+        state.loading = false;
+        state.users = action.payload;
+      })
+      .addCase(fetchUsers.rejected, (state) => {
+        state.loading = false;
+        state.error = "Failed to fetch users";
+      });
+  },
+});
+
+export default userSlice.reducer;
